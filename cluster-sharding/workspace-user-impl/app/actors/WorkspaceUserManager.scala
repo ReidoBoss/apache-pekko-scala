c3b3d167e9994e2697169460cdd29218ed2068a2
ReidoBoss <bryansagarino222@gmail.com>
2025-03-17 20:23:51 +0800

    Update WorkspaceUserManager.scala

--- cluster-sharding/workspace-user-impl/app/actors/WorkspaceUserManager.scala
+++ cluster-sharding/workspace-user-impl/app/actors/WorkspaceUserManager.scala
@@ -6,34 +6,41 @@ import org.apache.pekko.actor.typed.scaladsl.Behaviors
 
 object WorkspaceUserManager {
   sealed trait Action
-  final case class Create(actor: (IdUser, ActorRef[UserActor.Action]))
+  final case class Create(user: (IdUser, ActorRef[WorkspaceUserManager.Action]))
       extends Action
-  final case class Remove(actor: (IdUser, ActorRef[UserActor.Action]))
+  final case class Remove(user: ActorRef[WorkspaceUserManager.Action])
       extends Action
+  final case class Get(
+      replyTo: ActorRef[Set[(IdUser, ActorRef[WorkspaceUserManager.Action])]]
+  ) extends Action
   case object Terminate extends Action
 
   def apply(idWorkspace: IdWorkspace): Behavior[Action] = {
-    def states(
-        actors: Seq[(IdUser, ActorRef[UserActor.Action])]
-    ): Behavior[Action] = {
-      Behaviors.setup { context =>
-        Behaviors.receiveMessage[Action] {
 
-          case Create(actor) =>
-            states(actors :+ actor)
+    Behaviors.receive { (context, message) =>
+      def states(
+          users: Set[(IdUser, ActorRef[WorkspaceUserManager.Action])]
+      ): Behavior[Action] = {
+        message match
+          case Get(replyTo) =>
+            replyTo ! users
+            Behaviors.same
 
-          case Remove(actor) if actors.length == 1 =>
+          case Create(user) =>
+            states(users + user)
+
+          case Remove(user) if users.size == 1 =>
             Behaviors.stopped
 
           case Remove(actor) =>
-            states(actors.filter(_ != actor))
+            states(users.filter(_._2 != actor))
 
           case Terminate =>
             Behaviors.stopped
 
-        }
       }
+      states(Set.empty)
     }
-    states(Seq.empty)
+
   }
 }
