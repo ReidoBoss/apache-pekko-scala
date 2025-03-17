b5f989e9c3373713db9bee4aef7040cf141705dc
ReidoBoss <bryansagarino222@gmail.com>
2025-03-17 16:33:47 +0800

    Create WebSocketController.scala

---
+++ cluster-sharding/workspace-user-impl/app/controllers/WebSocketController.scala
@@ -0,0 +1,51 @@
+package controllers
+
+import javax.inject.*
+import java.util.UUID
+
+import scala.concurrent.duration.*
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+import play.api.libs.json.*
+import play.api.mvc.*
+import play.api.mvc.Results.*
+import play.api.libs.streams.ActorFlow
+
+import org.apache.pekko.NotUsed
+import org.apache.pekko.actor.typed.ActorRef
+import org.apache.pekko.actor.typed.Scheduler
+import org.apache.pekko.actor.typed.scaladsl.AskPattern.Askable
+import org.apache.pekko.stream.scaladsl.*
+import org.apache.pekko.util.Timeout
+import org.apache.pekko.actor.typed.scaladsl.adapter.*
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.cluster.sharding.ClusterSharding
+
+import actors.*
+
+@Singleton
+class WebSocketController @Inject() (
+    actorSystem: ActorSystem
+)(using ExecutionContext, Scheduler) {
+
+  given Timeout = 30.seconds
+
+  def index: WebSocket = WebSocket.acceptOrResult { request =>
+    val idFromHeader = request.id.toString()
+    request.session.get("idUser") match
+      case None => Future.successful(Left(Forbidden("Forbidden")))
+      case Some(idUser) =>
+        wsFutureFlow(idUser, idFromHeader)
+          .map(Right(_))
+  }
+
+  private def wsFutureFlow(
+      idUser: String,
+      idFromHeader: String,
+  ): Future[Flow[JsValue, JsValue, NotUsed]] = {
+    val actorName = s"actor-$idFromHeader-$idUser"
+    val userActor = actorSystem.spawn(UserActor.apply(), actorName)
+    userActor.ask(UserActor.Create(_))
+  }
+}
