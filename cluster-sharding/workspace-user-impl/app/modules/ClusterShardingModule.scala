807425e5d467b8a67abfb8fdb0efddb79f2a3ee2
ReidoBoss <bryansagarino222@gmail.com>
2025-03-17 16:33:41 +0800

    Create ClusterShardingModule.scala

---
+++ cluster-sharding/workspace-user-impl/app/modules/ClusterShardingModule.scala
@@ -0,0 +1,36 @@
+package modules
+
+import play.api.libs.concurrent.PekkoGuiceSupport
+
+import org.apache.pekko.actor.typed.scaladsl.adapter.*
+import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.cluster.sharding.typed.scaladsl.ClusterSharding
+import org.apache.pekko.actor.typed.ActorRef
+import org.apache.pekko.cluster.sharding.typed.ShardingEnvelope
+import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityTypeKey
+import org.apache.pekko.cluster.sharding.typed.scaladsl.Entity
+
+import actors.UserManager
+
+import com.google.inject.AbstractModule
+import com.google.inject.Provides
+import com.google.inject.Singleton
+
+class ClusterShardingModule extends AbstractModule with PekkoGuiceSupport {
+
+  @Provides
+  @Singleton
+  def provideClusterSharding(
+      system: ActorSystem[?]
+  ): ClusterSharding = ClusterSharding(system)
+
+  @Provides
+  @Singleton
+  def provideUserManager(
+      sharding: ClusterSharding
+  ): ActorRef[ShardingEnvelope[UserManager.Action]] =
+    val TypeKey = EntityTypeKey[UserManager.Action]("user-actor-manager")
+    sharding.init(
+      Entity(TypeKey)(entityContext => UserManager(entityContext.entityId))
+    )
+}
