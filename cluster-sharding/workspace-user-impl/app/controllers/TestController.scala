08867210f33887ab1b589ffb2f64aa281af1d8bd
ReidoBoss <bryansagarino222@gmail.com>
2025-03-17 16:33:49 +0800

    Update TestController.scala

--- cluster-sharding/workspace-user-impl/app/controllers/TestController.scala
+++ cluster-sharding/workspace-user-impl/app/controllers/TestController.scala
@@ -13,9 +13,6 @@ import org.apache.pekko
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.actor.typed.ActorRef
 import org.apache.pekko.cluster.sharding.typed.scaladsl.Entity
-
-import actors.*
-
 import pekko.actor.typed.Scheduler
 import pekko.actor.typed.scaladsl.adapter.*
 import pekko.actor.typed.scaladsl.AskPattern.Askable
@@ -25,29 +22,26 @@ import pekko.cluster.sharding.typed.scaladsl.ClusterSharding
 import pekko.cluster.sharding.typed.scaladsl.EntityTypeKey
 import pekko.cluster.sharding.typed.scaladsl.EntityRef
 
+import actors.*
+
 @Singleton
 class TestController @Inject() (
     val controllerComponents: ControllerComponents,
-    system: ActorSystem[?],
+    sharding: ClusterSharding
 )(using Scheduler, ExecutionContext)
     extends BaseController {
-
-  private val sharding  = ClusterSharding(system)
-  private val sharding2 = ClusterSharding(system)
 
   given Timeout = 1.seconds
 
   val TypeKey = EntityTypeKey[Counter.Command]("Counter")
-
-  sharding.init(
-    Entity(TypeKey)(createBehavior = entityContext =>
-      println(entityContext.entityId)
-      Counter(entityContext.entityId)
+  // val TypeKey2 = EntityTypeKey[Counter.Command]("Counter")
+  val test = sharding.init(
+    Entity(TypeKey)(createBehavior =
+      entityContext => Counter(entityContext.entityId)
     )
   )
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    sharding.entityRefFor(TypeKey, "counter-1") ! Counter.Increment
     Ok(Json.obj("message" -> "connected"))
   }
 
