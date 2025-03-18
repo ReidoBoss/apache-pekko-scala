1ff60b06443c37eb4abec376b7be814f85542c4b
ReidoBoss <bryansagarino222@gmail.com>
2025-03-18 09:31:12 +0800

    Update WorkspaceUserManagerService.scala

--- cluster-sharding/workspace-user-impl/app/actors/service/WorkspaceUserManagerService.scala
+++ cluster-sharding/workspace-user-impl/app/actors/service/WorkspaceUserManagerService.scala
@@ -3,14 +3,11 @@ package service
 
 import javax.inject.*
 
-import org.apache.pekko.actor.typed.scaladsl.adapter.*
-import org.apache.pekko.actor.typed.ActorSystem
-import org.apache.pekko.cluster.sharding.typed.scaladsl.ClusterSharding
-import org.apache.pekko.actor.typed.ActorRef
-import org.apache.pekko.cluster.sharding.typed.ShardingEnvelope
-import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityTypeKey
-import org.apache.pekko.cluster.sharding.typed.scaladsl.Entity
-import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityRef
+import org.apache.pekko
+import pekko.cluster.sharding.typed.scaladsl.ClusterSharding
+import pekko.cluster.sharding.typed.scaladsl.EntityTypeKey
+import pekko.cluster.sharding.typed.scaladsl.Entity
+import pekko.cluster.sharding.typed.scaladsl.EntityRef
 
 import impl.*
 
