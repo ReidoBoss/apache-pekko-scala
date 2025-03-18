4b06b0ed7310e9fecde1da510da410293f44ea67
ReidoBoss <bryansagarino222@gmail.com>
2025-03-18 08:47:48 +0800

    Update UserManagerService.scala

--- cluster-sharding/workspace-user-impl/app/actors/service/UserManagerService.scala
+++ cluster-sharding/workspace-user-impl/app/actors/service/UserManagerService.scala
@@ -3,5 +3,24 @@ package service
 
 import javax.inject.*
 
+import org.apache.pekko.actor.typed.scaladsl.adapter.*
+import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.cluster.sharding.typed.scaladsl.ClusterSharding
+import org.apache.pekko.actor.typed.ActorRef
+import org.apache.pekko.cluster.sharding.typed.ShardingEnvelope
+import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityTypeKey
+import org.apache.pekko.cluster.sharding.typed.scaladsl.Entity
+import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityRef
+
 @Singleton
-class UserManagerService @Inject() {}
+class UserManagerService @Inject (sharding: ClusterSharding) {
+  private val UserManagerTypeKey =
+    EntityTypeKey[UserManager.Action]("UserManager")
+
+  sharding.init(
+    Entity(UserManagerTypeKey)(createBehavior =
+      entityContext => UserManager(IdUser.fromString(entityContext.entityId))
+    )
+  )
+
+}
