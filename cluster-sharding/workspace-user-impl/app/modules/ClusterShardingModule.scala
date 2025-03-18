ac36a3eb2ca2b761552a469767b25a63e388bffd
ReidoBoss <bryansagarino222@gmail.com>
2025-03-18 09:31:17 +0800

    Update ClusterShardingModule.scala

--- cluster-sharding/workspace-user-impl/app/modules/ClusterShardingModule.scala
+++ cluster-sharding/workspace-user-impl/app/modules/ClusterShardingModule.scala
@@ -1,23 +1,13 @@
 package modules
 
-import play.api.libs.concurrent.PekkoGuiceSupport
-
-import org.apache.pekko.actor.typed.scaladsl.adapter.*
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.cluster.sharding.typed.scaladsl.ClusterSharding
-import org.apache.pekko.actor.typed.ActorRef
-import org.apache.pekko.cluster.sharding.typed.ShardingEnvelope
-import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityTypeKey
-import org.apache.pekko.cluster.sharding.typed.scaladsl.Entity
-import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityRef
-
-import actors.*
 
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import com.google.inject.Singleton
 
-class ClusterShardingModule extends AbstractModule with PekkoGuiceSupport {
+class ClusterShardingModule extends AbstractModule {
 
   /**
    * ClusterSharding Module for Dependency Injection
