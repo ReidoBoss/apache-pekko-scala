a7fafbb42e9e4e03bfbd9c7865c139bdea87586c
ReidoBoss <bryansagarino222@gmail.com>
2025-03-18 08:53:44 +0800

    Update WorkspaceUserManagerService.scala

--- cluster-sharding/workspace-user-impl/app/actors/service/WorkspaceUserManagerService.scala
+++ cluster-sharding/workspace-user-impl/app/actors/service/WorkspaceUserManagerService.scala
@@ -13,4 +13,16 @@ import org.apache.pekko.cluster.sharding.typed.scaladsl.Entity
 import org.apache.pekko.cluster.sharding.typed.scaladsl.EntityRef
 
 @Singleton
-class WorkspaceUserManagerService @Inject (sharding: ClusterSharding) {}
+class WorkspaceUserManagerService @Inject (sharding: ClusterSharding) {
+  private val WorkspaceUserManagerTypeKey =
+    EntityTypeKey[WorkspaceUserManager.Action]("WorkspaceUserManager")
+
+  sharding.init(
+    Entity(WorkspaceUserManagerTypeKey)(createBehavior =
+      entityContext =>
+        WorkspaceUserManager(IdWorkspace.fromString(entityContext.entityId))
+    )
+  )
+  def get(id: IdWorkspace): EntityRef[WorkspaceUserManager.Action] =
+    sharding.entityRefFor(WorkspaceUserManagerTypeKey, id.toString())
+}
